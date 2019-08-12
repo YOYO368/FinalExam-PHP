79471fb1351c11fe41be07f3a80216ca602d19d0
YOYO368 <yoyo368@ess.ais.ac.nz>
2019-08-12 14:17:07 +1200

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "us-cdbr-iron-east-02.cleardb.net";
-	$database = "heroku_a26af63ef5ce9d3";
-	$user = "b459e6bfc271b1";
-	$password = "3ea494f1";
+	$host = "examapplicationphp-mysqldbserver.mysql.database.azure.com";
+	$database = "dbfinalexam";
+	$user = "yye2942@examapplicationphp-mysqldbserver";
+	$password = "skatkwkd309^^";
 
 	$connection = mysqli_connect($host, $user, $password, $database);
 ?>
