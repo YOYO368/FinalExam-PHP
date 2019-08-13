df4e0593a4ccba2bed98b5683b31ff470e0279b5
YOYO368 <yoyo368@ess.ais.ac.nz>
2019-08-13 19:23:20 +1200

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
 	
-	$host = "finalappphp-mysqldbserver.mysql.database.azure.com";
+	$host = "applicationphp-mysqldbserver.mysql.database.azure.com";
 	$database = "dbFinalExam";
-	$user = "yye2942@finalappphp-mysqldbserver";
+	$user = "yye2942@applicationphp-mysqldbserver";
 	$password = "skatkwkd309^^";
 
 	$connection = mysqli_connect($host, $user, $password, $database);
